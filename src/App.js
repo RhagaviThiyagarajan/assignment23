71ec8e2450cc6eed28b769261b512d5b1cdf2baf
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2022-08-12 21:50:53 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from "react";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
-import DoneIcon from '@mui/icons-material/Done';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Form from 'react-bootstrap/Form';
